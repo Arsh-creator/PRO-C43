81b0d23b91bb7c91f875198858124423c42386c0
Arsh-creator <75788096+Arsh-creator@users.noreply.github.com>
2021-05-05 15:19:21 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,33 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+   if(score === 0){
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70);
+
+   }
+    
+   if(score ===1){
+    fill("white")
+    textSize(15)
+    text("CUTNIFON", 700,150)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 700,170);
+
+   }
+    if(score === 2){
+    fill("white")
+    textSize(15)
+    text("ATEDASBA", 100,310)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,330);
+
+    }
+
+}
